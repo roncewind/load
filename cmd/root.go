6578e688a1e17c347a162889c470f22783ca7e27
roncewind <dad@lynntribe.net>
2022-06-23 11:35:36 -0700

    update to read from queue

--- cmd/root.go
+++ cmd/root.go
@@ -6,19 +6,28 @@ package cmd
 
 import (
 	"fmt"
+	"net"
+	// "net/http"
+	"net/url"
 	"os"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+	// read from this URL, could be a file or a queue
+	inputURL  string
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
 	Use:   "load",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
+	Short: "Load records into Senzing",
+	Long: `TODO: A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
@@ -27,14 +36,19 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-
+		fmt.Println("start Run")
+		for _, key := range viper.AllKeys() {
+			fmt.Println(key, " = ", viper.Get(key))
+		}
+		parseURL(viper.GetString("inputURL"))
+		read(viper.GetString("inputURL"))
 	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -47,11 +61,14 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.load.yaml)")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.senzing/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().StringVarP(&inputURL, "inputURL", "i", "", "input location")
+	viper.BindPFlag("inputURL", RootCmd.Flags().Lookup("inputURL"))
+
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -64,16 +81,96 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".load" (without extension).
+		// Search config in <home directory>/.senzing with name "config" (without extension).
+		viper.AddConfigPath(home+"/.senzing-tools")
 		viper.AddConfigPath(home)
+		viper.AddConfigPath("/etc/senzing-tools")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".load")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
+	// all env vars should be prefixed with "SENZING_TOOLS_"
+	viper.SetEnvPrefix("senzing_tools")
+	viper.BindEnv("inputURL")
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+func parseURL(urlString string) {
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\tScheme: ", u.Scheme)
+	fmt.Println("\tUser full: ", u.User)
+	fmt.Println("\tUser name: ", u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println("\tPassword: ", p)
+
+	fmt.Println("\tHost full: ", u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println("\tHost: ", host)
+	fmt.Println("\tPort: ", port)
+
+	fmt.Println("\tPath: ", u.Path)
+	fmt.Println("\tFragment: ", u.Fragment)
+
+	fmt.Println("\tQuery string: ", u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println("\tParsed query string: ", m)
+	// fmt.Println(m["k"][0])
+}
+
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		s := fmt.Sprintf("%s: %s", msg, err)
+		panic(s)
+	}
+}
+
+func read(urlString string) {
+	conn, err := amqp.Dial(urlString)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+	  "hello", // name
+	  false,   // durable
+	  false,   // delete when unused
+	  false,   // exclusive
+	  false,   // no-wait
+	  nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Received a message: %s\n", d.Body)
+		}
+	}()
+
+	fmt.Println(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
