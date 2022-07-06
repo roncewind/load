3551f0c206ad3b7ece6c4210eccbf251d2136a0b
roncewind <dad@lynntribe.net>
2022-07-06 15:08:35 -0700

    updated validation

--- cmd/root.go
+++ cmd/root.go
@@ -174,6 +174,8 @@ func read(urlString string) {
 			if valid {
 				//TODO: Senzing here
 			} else {
+				// FIXME: errors should be specific to the input method
+				//  ala rabbitmq message ID?
 				fmt.Println("Error with message:", err)
 			}
 		}
@@ -184,27 +186,29 @@ func read(urlString string) {
 }
 
 // ----------------------------------------------------------------------------
-type Entity struct {
+type Record struct {
 	DataSource string `json:"DATA_SOURCE"`
 	RecordId string `json:"RECORD_ID"`
 }
 
 // ----------------------------------------------------------------------------
 func validateLine(line string) (bool, error) {
-	var entity Entity
-	valid := json.Unmarshal([]byte(line), &entity) == nil
+	var record Record
+	valid := json.Unmarshal([]byte(line), &record) == nil
 	if valid {
-		return validateEntity(entity)
+		return validateRecord(record)
 	}
 	return valid, errors.New("JSON-line not well formed.")
 }
 
 // ----------------------------------------------------------------------------
-func validateEntity(entity Entity) (bool, error) {
-	if entity.DataSource == "" {
+func validateRecord(record Record) (bool, error) {
+	// FIXME: errors should be specific to the input method
+	//  ala rabbitmq message ID?
+	if record.DataSource == "" {
 		return false, errors.New("A DATA_SOURCE field is required.")
 	}
-	if entity.RecordId == "" {
+	if record.RecordId == "" {
 		return false, errors.New("A RECORD_ID field is required.")
 	}
 	return true, nil
