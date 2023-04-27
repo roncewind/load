411788d1002b94ae3a713eca28015525d16b6cf0
roncewind <dad@lynntribe.net>
2023-04-27 11:26:23 -0700

    save point

--- cmd/root.go
+++ cmd/root.go
@@ -4,18 +4,25 @@ Copyright © 2022 roncewind <dad@lynntribe.net>
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/senzing/go-logging/messagelogger"
+	"github.com/docktermj/go-xyzzy-helpers/logger"
+	"github.com/roncewind/load/loader"
+	"github.com/senzing/senzing-tools/constant"
+	"github.com/senzing/senzing-tools/envar"
+	"github.com/senzing/senzing-tools/helper"
+	"github.com/senzing/senzing-tools/option"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 const (
 	defaultDelayInSeconds int    = 0
+	defaultEngineConfig   string = ""
 	defaultFileType       string = ""
 	defaultInputURL       string = ""
 	defaultOutputURL      string = ""
@@ -32,16 +39,11 @@ const (
 	load --input-url "https://public-read-access.s3.amazonaws.com/TestDataSets/SenzingTruthSet/truth-set-3.0.0.jsonl"
 `
 )
+
 const (
-	delayInSeconds         string = "delay-in-seconds"
-	engineConfigParameter  string = "engine-configuration-json"
-	envVarPrefix           string = "SENZING_TOOLS"
-	envVarReplacerCharNew  string = "_"
-	envVarReplacerCharOld  string = "-"
-	inputFileTypeParameter string = "input-file-type"
-	inputURLParameter      string = "input-url"
-	logLevelParameter      string = "log-level"
-	withInfoParameter      string = "with-info"
+	envVarReplacerCharNew string = "_"
+	envVarReplacerCharOld string = "-"
+	withInfoParameter     string = "with-info"
 )
 
 var (
@@ -51,73 +53,78 @@ var (
 )
 
 var (
-	cfgFile string
-	delay   int = 0
-	// engineConfigJson string
-	exchange   string = "senzing"
-	fileType   string //TODO: load from file
-	inputQueue string = "senzing_input"
-	// inputURL         string // read from this URL, could be a file or a queue
-	// logLevel         string = "info"
-	msglog   messagelogger.MessageLoggerInterface
-	withInfo bool = false
+	defaultWithInfoParameter string = ""
 )
 
 // load is 6201:  https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 const productIdentifier = 6201
 
-var idMessages = map[int]string{
-	1:    "Viper key list:",
-	2:    "  - %s = %s",
-	11:   "%s has a score of %d.",
-	999:  "A test of INFO.",
-	1000: "A test of WARN.",
-	2000: "A test of ERROR.",
-	2001: "Config file found, but not loaded",
-}
-
 // ----------------------------------------------------------------------------
-// RootCmd represents the base command when called without any subcommands
+// Command
+// ----------------------------------------------------------------------------
+
+// RootCmd represents the command.
 var RootCmd = &cobra.Command{
-	Use:   Use,
-	Short: Short,
-	Long:  Long,
-	PreRun: func(cmd *cobra.Command, args []string) {
-		viper.BindPFlag(delayInSeconds, cmd.Flags().Lookup(delayInSeconds))
-		viper.BindPFlag(engineConfigParameter, cmd.Flags().Lookup(engineConfigParameter))
-		viper.BindPFlag(inputFileTypeParameter, cmd.Flags().Lookup(inputFileTypeParameter))
-		viper.BindPFlag(inputURLParameter, cmd.Flags().Lookup(inputURLParameter))
-		viper.BindPFlag(logLevelParameter, cmd.Flags().Lookup(logLevelParameter))
-		viper.BindPFlag(withInfoParameter, cmd.Flags().Lookup(withInfoParameter))
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		// if msglog.IsInfo() {
-		// msglog.Log(1, logger.LevelInfo)
-		fmt.Println("Viper keys")
-		for _, key := range viper.AllKeys() {
-			// msglog.Log(2, key, viper.Get(key), logger.LevelInfo)
-			fmt.Println(key, ":", viper.Get(key))
-		}
-		// }
-		fmt.Println(time.Now(), "Sleep for ", delay, " seconds to let queues and database settle down and come up.")
-		time.Sleep(time.Duration(delay) * time.Second)
-
-		// ctx := context.Background()
-
-		// loader := &loader.LoaderImpl{
-		// 	InputURL:         viper.GetString(inputURLParameter),
-		// 	LogLevel:         viper.GetString(logLevelParameter),
-		// 	EngineConfigJson: viper.GetString(engineConfigParameter),
-		// }
-
-		// if !loader.Load(ctx) {
-		// 	cmd.Help()
-		// }
-
-	},
+	Use:     Use,
+	Short:   Short,
+	Long:    Long,
+	PreRun:  PreRun,
+	Run:     Run,
+	Version: Version(),
 }
 
 // ----------------------------------------------------------------------------
+
+// Used in construction of cobra.Command
+func PreRun(cobraCommand *cobra.Command, args []string) {
+	loadConfigurationFile(cobraCommand)
+	loadOptions(cobraCommand)
+	cobraCommand.SetVersionTemplate(constant.VersionTemplate)
+}
+
+// ----------------------------------------------------------------------------
+
+// The core of this command
+func Run(cmd *cobra.Command, args []string) {
+	fmt.Println("start Run")
+	fmt.Println("viper key list:")
+	for _, key := range viper.AllKeys() {
+		fmt.Println("  - ", key, " = ", viper.Get(key))
+	}
+	setLogLevel()
+	fmt.Println(time.Now(), "Sleep for", viper.GetInt(option.DelayInSeconds), "seconds to let RabbitMQ and Postgres settle down and come up.")
+	time.Sleep(time.Duration(viper.GetInt(option.DelayInSeconds)) * time.Second)
+
+	if viper.IsSet(option.InputURL) {
+
+		ctx := context.Background()
+
+		loader := &loader.LoaderImpl{
+			InputURL:         viper.GetString(option.InputURL),
+			LogLevel:         viper.GetString(option.LogLevel),
+			EngineConfigJson: viper.GetString(option.EngineConfigurationJson),
+		}
+
+		if !loader.Load(ctx) {
+			cmd.Help()
+		}
+
+	} else {
+		cmd.Help()
+		fmt.Println("Build Version:", buildVersion)
+		fmt.Println("Build Iteration:", buildIteration)
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// Used in construction of cobra.Command
+func Version() string {
+	return helper.MakeVersion(githubVersion, githubIteration)
+}
+
+// ----------------------------------------------------------------------------
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
@@ -129,77 +136,105 @@ func Execute() {
 
 // ----------------------------------------------------------------------------
 func init() {
-	msglog, _ = messagelogger.NewSenzingLogger(productIdentifier, idMessages)
-	cobra.OnInitialize(initConfig)
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.senzing/config.yaml)")
-
-	// local flags for load
-	RootCmd.Flags().IntVar(&delay, delayInSeconds, 0, "time to wait before start of processing")
-	RootCmd.Flags().StringVar(&engineConfigJson, engineConfigParameter, "", "Senzing engine configuration JSON")
-	RootCmd.Flags().StringVar(&fileType, inputFileTypeParameter, "", "file type override")
-	RootCmd.Flags().StringVarP(&inputURL, inputURLParameter, "i", "", "input location")
-	RootCmd.Flags().StringVar(&logLevel, logLevelParameter, "", "set the logging level, default Error")
-	RootCmd.Flags().Bool(withInfoParameter, false, "set to add record withInfo")
+	RootCmd.Flags().Int(option.DelayInSeconds, defaultDelayInSeconds, option.DelayInSecondsHelp)
+	RootCmd.Flags().String(option.InputFileType, defaultFileType, option.InputFileTypeHelp)
+	RootCmd.Flags().String(option.InputURL, defaultInputURL, option.InputURLHelp)
+	RootCmd.Flags().String(option.LogLevel, defaultLogLevel, fmt.Sprintf(option.LogLevelHelp, envar.LogLevel))
+	RootCmd.Flags().String(option.OutputURL, defaultOutputURL, option.OutputURLHelp)
 }
 
 // ----------------------------------------------------------------------------
-// initConfig reads in config file and ENV variables if set.
-// Config precedence:
-// - cmdline args
-// - env vars
-// - config file
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
+
+// If a configuration file is present, load it.
+func loadConfigurationFile(cobraCommand *cobra.Command) {
+	configuration := ""
+	configFlag := cobraCommand.Flags().Lookup(option.Configuration)
+	if configFlag != nil {
+		configuration = configFlag.Value.String()
+	}
+	if configuration != "" { // Use configuration file specified as a command line option.
+		viper.SetConfigFile(configuration)
+	} else { // Search for a configuration file.
+
+		// Determine home directory.
+
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in <home directory>/.senzing with name "config" (without extension).
+		// Specify configuration file name.
+
+		viper.SetConfigName("move")
+		viper.SetConfigType("yaml")
+
+		// Define search path order.
+
 		viper.AddConfigPath(home + "/.senzing-tools")
 		viper.AddConfigPath(home)
 		viper.AddConfigPath("/etc/senzing-tools")
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("config")
 	}
 
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error
-		} else {
-			// Config file was found but another error was produced
-			msglog.Log(2001, err)
-		}
+	// If a config file is found, read it in.
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Applying configuration file:", viper.ConfigFileUsed())
 	}
-	viper.AutomaticEnv() // read in environment variables that match
-	// all env vars should be prefixed with "SENZING_TOOLS_"
+}
+
+// ----------------------------------------------------------------------------
+
+// Configure Viper with user-specified options.
+func loadOptions(cobraCommand *cobra.Command) {
+	viper.AutomaticEnv()
 	replacer := strings.NewReplacer(envVarReplacerCharOld, envVarReplacerCharNew)
 	viper.SetEnvKeyReplacer(replacer)
-	viper.SetEnvPrefix(envVarPrefix)
-	viper.BindEnv(delayInSeconds)
-	viper.BindEnv(engineConfigParameter)
-	viper.BindEnv(inputFileTypeParameter)
-	viper.BindEnv(inputURLParameter)
-	viper.BindEnv(logLevelParameter)
-	viper.BindEnv(withInfoParameter)
+	viper.SetEnvPrefix(constant.SetEnvPrefix)
 
-	viper.SetDefault(delayInSeconds, 0)
-	viper.SetDefault(logLevelParameter, "error")
-	viper.SetDefault(withInfoParameter, false)
+	// Ints
 
-	// setup local variables, in case they came from a config file
-	//TODO:  why do I have to do this?  env vars and cmdline params get mapped
-	//  automatically, this is only IF the var is in the config file.
-	//  am i missing a way to bind config file vars to local vars?
-	delay = viper.GetInt(delayInSeconds)
-	// engineConfigJson = viper.GetString(engineConfigParameter)
-	fileType = viper.GetString(inputFileTypeParameter)
-	// inputURL = viper.GetString(inputURLParameter)
-	// logLevel = viper.GetString(logLevelParameter)
-	withInfo = viper.GetBool(withInfoParameter)
+	intOptions := map[string]int{
+		option.DelayInSeconds: defaultDelayInSeconds,
+	}
+	for optionKey, optionValue := range intOptions {
+		viper.SetDefault(optionKey, optionValue)
+		viper.BindPFlag(optionKey, cobraCommand.Flags().Lookup(optionKey))
+	}
 
-	msglog.SetLogLevelFromString(viper.GetString(logLevelParameter))
+	// Strings
+
+	stringOptions := map[string]string{
+		option.EngineConfigurationJson: defaultEngineConfig,
+		option.InputFileType:           defaultFileType,
+		option.InputURL:                defaultInputURL,
+		option.LogLevel:                defaultLogLevel,
+		withInfoParameter:              defaultWithInfoParameter,
+	}
+	for optionKey, optionValue := range stringOptions {
+		viper.SetDefault(optionKey, optionValue)
+		viper.BindPFlag(optionKey, cobraCommand.Flags().Lookup(optionKey))
+	}
+
+}
+
+// ----------------------------------------------------------------------------
+func setLogLevel() {
+	var level logger.Level = logger.LevelError
+	if viper.IsSet(option.LogLevel) {
+		switch strings.ToUpper(viper.GetString(option.LogLevel)) {
+		case logger.LevelDebugName:
+			level = logger.LevelDebug
+		case logger.LevelErrorName:
+			level = logger.LevelError
+		case logger.LevelFatalName:
+			level = logger.LevelFatal
+		case logger.LevelInfoName:
+			level = logger.LevelInfo
+		case logger.LevelPanicName:
+			level = logger.LevelPanic
+		case logger.LevelTraceName:
+			level = logger.LevelTrace
+		case logger.LevelWarnName:
+			level = logger.LevelWarn
+		}
+		logger.SetLevel(level)
+	}
 }
